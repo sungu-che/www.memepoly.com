bc28073af4f31d489be6ec44339b3985d44cfd6a
oauth-network <127634415+oauth-network@users.noreply.github.com>
2024-08-03 01:05:50 +0900

    Update

---
+++ src/features.js
@@ -0,0 +1,132 @@
+window.Features = {
+	OCEAN : {
+		color : "#44447a",
+		key : "",
+		value : ""
+	},
+	COAST : {
+		color : "#33335a",
+		key : "",
+		value : ""
+	},
+	LAKESHORE : {
+		color : "#225588",
+		key : "",
+		value : ""
+	},
+	LAKE : {
+		color : "#336699",
+		key : "",
+		value : ""
+	},
+	RIVER : {
+		color : "#225588",
+		key : "",
+		value : ""
+	},
+	MARSH : {
+		color : "#2f6666",
+		key : "",
+		value : ""
+	},
+	ICE : {
+		color : "#99ffff",
+		key : "",
+		value : ""
+	},
+	BEACH : {
+		color : "#a09077",
+		key : "",
+		value : ""
+	},
+	ROAD1 : {
+		color : "#442211",
+		key : "",
+		value : ""
+	},
+	ROAD2 : {
+		color : "#553322",
+		key : "",
+		value : ""
+	},
+	ROAD3 : {
+		color : "#664433",
+		key : "",
+		value : ""
+	},
+	BRIDGE : {
+		color : "#686860",
+		key : "",
+		value : ""
+	},
+	LAVA : {
+		color : "#cc3333",
+		key : "",
+		value : ""
+	},
+	SNOW : {
+		color : "#ffffff",
+		key : "",
+		value : ""
+	},
+	TUNDRA : {
+		color : "#bbbbaa",
+		key : "",
+		value : ""
+	},
+	BARE : {
+		color : "#888888",
+		key : "",
+		value : ""
+	},
+	SCORCHED : {
+		color : "#555555",
+		key : "",
+		value : ""
+	},
+	TAIGA : {
+		color : "#99aa77",
+		key : "",
+		value : ""
+	},
+	SHRUBLAND : {
+		color : "#889977",
+		key : "",
+		value : ""
+	},
+	TEMPERATE_DESERT : {
+		color : "#c9d29b",
+		key : "",
+		value : ""
+	},
+	TEMPERATE_RAIN_FOREST : {
+		color : "#448855",
+		key : "",
+		value : ""
+	},
+	TEMPERATE_DECIDUOUS_FOREST : {
+		color : "#679459",
+		key : "",
+		value : ""
+	},
+	GRASSLAND : {
+		color : "#88aa55",
+		key : "",
+		value : ""
+	},
+	SUBTROPICAL_DESERT : {
+		color : "#d2b98b",
+		key : "",
+		value : ""
+	},
+	TROPICAL_RAIN_FOREST : {
+		color : "#337755",
+		key : "",
+		value : ""
+	},
+	TROPICAL_SEASONAL_FOREST : {
+		color : "#559944",
+		key : "",
+		value : ""
+	}
+}
